f1bb91b23b8af359195a255c16613fce81144a89
hida <hidayatulkhasanah760@gmail.com>
2023-09-06 18:56:00 +0700

    merubah database

--- connection.php
+++ connection.php
@@ -1,11 +1,16 @@
 <?php
 
 $host = 'localhost'; //127.0.0.1
-$database_name = 'native_test';
+$database_name = 'db_perpus';
 $database_username = 'root';
 $database_password = '';
 
 // Membuat koneksi menuju database
-$mysqli = mysqli_connect($host, $database_username, $database_password, $database_name);
+$connect = mysqli_connect($host, $database_username, $database_password, $database_name);
 
+if ($connect){
+    echo "connected";
+}else {
+    echo "not connected";
+}
 ?>
